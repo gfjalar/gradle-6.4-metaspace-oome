53426103f4b244062e2ccdcda81913d5d4b743ee
Piotr Galar <pgalar@sumologic.com>
2020-05-13 08:25:17 +0200

    Simplify reproduction scenario

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 ###### Commands
 
-1. `bin/generate.sh 100 100 100`
+1. `bin/generate.sh 100`
 1. `./gradlew clean assemble --no-build-cache --scan`
--- bin/generate.sh
+++ bin/generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Usage: bin/generate.sh [# of subprojects] [# of classes per subproject] [# of methods per class]"
+echo "Usage: bin/generate.sh [# of subprojects]"
 
 BIN_DIR=`dirname "${BASH_SOURCE[0]}"`
 ROOT_DIR=`dirname "$BIN_DIR"`
@@ -9,28 +9,12 @@ GENERATED_DIR="$ROOT_DIR/generated"
 rm -rf "$GENERATED_DIR"
 mkdir "$GENERATED_DIR"
 
-BODY=""
-for (( method=1; method <= $3; method++ )); do
-  BODY="${BODY}N  def template$method(): Unit = { println(\"Template method #$method\") }N"
-done
-
 pushd "$ROOT_DIR"
 
-for (( subproject=1; subproject <= $1; subproject++ )); do
+for subproject in $(seq -w 1 $1); do
   cp -r template "$GENERATED_DIR/subproject$subproject"
-  pushd "$GENERATED_DIR/subproject$subproject/src/main/scala/gradle/metaspace/oome"
-
-  sed -i '' "s|{}|{$BODY}|" "Template.scala"
-  sed -i '' $'s/N/\\\n/g' "Template.scala"
-  for (( class=1; class <= $2; class++ )); do
-    cp "Template.scala" "Template$class.scala"
-    sed -i '' "s|class Template {|class Template$class {|" "Template$class.scala"
-  done
-
-  rm "Template.scala"
-  popd
 done
 
 popd
 
-echo "Generated $1 subprojects with $2 classes per subproject and $3 methods per class"
+echo "Generated $1 subprojects"
